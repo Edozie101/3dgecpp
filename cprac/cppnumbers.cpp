3ce1652bcafa0576e9077248b03c9a2ffe411987
edzye101 <edzye101@gmail.com>
2015-11-10 13:28:20 +0000

    added two structs

---
+++ cprac/cppnumbers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+// using namespace std;
+
+struct Vector {
+  int x, y, z;
+
+};
+
+struct Object {
+
+  int x, y, z;
+  std::string name;
+  Vector d;
+
+};
+
+
+
+
+int main()
+{
+
+
+	std::cout << "Game Over!" << std::endl;
+  std::cout << "Statically typed languages have loopholes such as type punning which can circumvent the need to define a type" << std::endl;
+
+	return 0;
+
+
+// Game over man
+// Get used to it...
+
+}
